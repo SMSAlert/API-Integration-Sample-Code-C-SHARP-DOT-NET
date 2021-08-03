97bb3583c7a86e477b3160e3f2c1f9118b69930e
Madhur Khatavkar <77504674+madhurkhatavkar@users.noreply.github.com>
2021-08-03 19:40:52 +0530

    Update Smsalert.cs

--- Smsalert.cs
+++ Smsalert.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,17 +10,29 @@ using System.Net;
 /// </summary>
 public class Smsalert
 {
+    private string apikey;
+    private string username;
+    private string password;
     private string authkey;
 
-	public Smsalert(string authkey)
-	{
-        this.authkey = authkey;
-	}
-
-    public string sendsms(string senderid, string mobileno, string text, string route) 
+    public Smsalert(string username, string password)
     {
+        this.username = username;
+        this.password = password;
+    }
+
+    public Smsalert(string apikey)
+    {
+        this.apikey = apikey;
+    }
+    public string sendsms(string senderid, string mobileno, string text, string route)
+    {
+        if(apikey!=null)
+            authkey="apikey="+ this.apikey;
+        else
+            authkey="user="+ this.username +"&pwd="+ this.password;  
         WebClient client = new WebClient();
-        string apiurl = "http://www.smsalert.co.in/api/push.json?apikey=" + this.authkey + "&sender=" + senderid + "&mobileno=" + mobileno + "&text=" + text + "&route=" + route ;
+        string apiurl = "http://www.smsalert.co.in/api/push.json?"+ authkey + "&sender=" + senderid + "&mobileno=" + mobileno + "&text=" + text + "&route=" + route;
         Stream data = client.OpenRead(apiurl);
         StreamReader reader = new StreamReader(data);
         string response = reader.ReadToEnd();
